d25ba8b49838bd6ef5c7cdb690290f23840e6c7f
12816 <aybolek27@gmail.com>
2022-10-13 23:54:21 -0500

    Revert "building user create feature"
    This reverts commit 30ffa33bec913bc8cc4785b1e25cb6cb18076198.

--- src/main/java/com/domain/bootstrap/DataGenerator.java
+++
@@ -1,4 +0,0 @@
-package com.domain.bootstrap;
-
-public class DataGenerator {
-}
--- src/main/java/com/domain/service/CrudService.java
+++ src/main/java/com/domain/service/CrudService.java
@@ -9,5 +9,6 @@ public interface CrudService<T, ID> {
     T save(T object);
     T findById(ID id);
     List<T> findAll();
+    void delete(T role);
     void deleteById(ID id);
 }
