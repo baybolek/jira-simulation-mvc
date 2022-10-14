8e5aa819dee1cb63c27c0ffd7517e4dfcff90626
12816 <aybolek27@gmail.com>
2022-10-13 23:51:29 -0500

    building user create feature

---
+++ src/main/java/com/domain/converter/UserDtoConverter.java
@@ -0,0 +1,25 @@
+package com.domain.converter;
+
+
+import com.domain.dto.RoleDTO;
+import com.domain.dto.UserDTO;
+import com.domain.service.UserService;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationPropertiesBinding
+public class UserDtoConverter implements Converter<String, UserDTO> {
+
+    UserService userService;
+
+    public UserDtoConverter(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDTO convert(String source) {
+        return userService.findById(source);
+    }
+}
--- src/main/java/com/domain/service/impl/UserServiceImpl.java
+++ src/main/java/com/domain/service/impl/UserServiceImpl.java
@@ -2,9 +2,11 @@ package com.domain.service.impl;
 
 import com.domain.dto.UserDTO;
 import com.domain.service.UserService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserServiceImpl extends AbstractMapService<UserDTO, String> implements UserService {
 
     @Override
@@ -25,5 +27,10 @@ public class UserServiceImpl extends AbstractMapService<UserDTO, String> impleme
     @Override
     public void deleteById(String id) {
         super.deleteById(id);
+    }
+
+    @Override
+    public void update(UserDTO object) {
+        super.update(object.getUserName(), object);
     }
 }
